7a7f9baf9037dbe6dbfc4af2b4306b88434686a1
Mateusz Borycki <mateuszborycki@gmail.com>
2015-11-25 16:16:08 +0100

    drawing bear using opengl

--- Bunnymark/main.cpp
+++ Bunnymark/main.cpp
@@ -105,28 +105,19 @@ void drawGL()
 
 	GLfloat vVertices[] = {  0.0f + x,  0.f + y, 0.0f,
 	                         -50.f+x, 0.f + y, 0.0f,
-	                          0.f+x, 100.f + y, 0.0f };
+	                          0.f+x, 100.f + y, 0.0f,
+													  -50.f+x, 100.f + y, 0.0f };
 
 	// No clientside arrays, so do this in a webgl-friendly manner
 
 	glBindBuffer(GL_ARRAY_BUFFER, vertexPosObject);
-	glBufferData(GL_ARRAY_BUFFER, 9*4, vVertices, GL_STATIC_DRAW);
-
-	// Set the viewport
-	//glViewport ( 0, 0, esContext->width, esContext->height );
-
-	// Clear the color buffer
-	//glClear ( GL_COLOR_BUFFER_BIT );
-
-	// Use the program object
-	//glUseProgram ( userData->programObject );
+	glBufferData(GL_ARRAY_BUFFER, 12*4, vVertices, GL_STATIC_DRAW);
 
 	// Load the vertex data
-	glBindBuffer(GL_ARRAY_BUFFER, vertexPosObject);
 	glVertexAttribPointer(0 /* ? */, 3, GL_FLOAT, 0, 0, 0);
 	glEnableVertexAttribArray(0);
 
-	glDrawArrays ( GL_TRIANGLES, 0, 3 );
+	glDrawArrays ( GL_TRIANGLE_STRIP, 0, 4 );
 
 	glDisableVertexAttribArray(0);
 }
